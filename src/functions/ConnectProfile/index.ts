434be040e0704c90b6712b568c013fac8b89d056
DopePanda Dev <dopepandaorg@protonmail.com>
2022-04-17 18:37:54 +0530

    Include a function to connect a user profile, insert into indexer

---
+++ src/functions/ConnectProfile/index.ts
@@ -0,0 +1,26 @@
+import schema from './schema';
+import { handlerPath } from '@libs/handler-resolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  timeout: 30,
+  memorySize: 128,
+  events: [
+    {
+      http: {
+        method: 'post',
+        path: 'ConnectProfile',
+        request: {
+          schemas: {
+            'application/json': schema,
+          },
+        },
+      },
+    },
+  ],
+  environment: {
+    HASURA_API: process.env.HASURA_API,
+    HASURA_SECRET: process.env.HASURA_SECRET,
+    PURESTAKE_API_KEY: process.env.PURESTAKE_API_KEY
+  }
+};
